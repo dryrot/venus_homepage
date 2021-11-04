a83cc0fd828b84056257319c590cd424afdb9ae3
jiwon <jiwonhwang@i-bricks.co.kr>
2021-11-04 22:27:22 +0900

    dryrot@venusdental.cafe24app.com:dryrot_venusdental

--- web.js
+++ web.js
@@ -1,7 +1,11 @@
-<html>
-<body>
-<script type="text/javascript">
-  document.write("JavaScript is a simple language for javatpoint learners");
-</script>
-</body>
-</html>;
+const express = require("express");
+const app = express();
+const PORT = 8001;
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.listen(PORT, () => {
+  console.log(`server started on PORT ${PORT}`);
+});
